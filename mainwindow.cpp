33b43107683f35719048b281c3b527a9c390ce1d
byronyi <yibairen1994@gmail.com>
2013-06-30 12:34:01 +0800

    Skeleton finished

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,69 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->inputGroupBox->setDisabled(true);
+    ui->executeButton->setDisabled(true);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionLoad_triggered()
+{
+    QStringList fileNames = QFileDialog::getOpenFileNames(this, tr("Load Training Set"),
+                                                          ".", tr("Training Set files (*.txt)"));
+
+
+    if(fileNames.empty())
+        return;
+
+    ui->log->append("Loading files...\n");
+
+    for(QStringList::Iterator iter = fileNames.begin(); iter != fileNames.end(); ++iter)
+        if(!loadFile(*iter))
+        {
+            ui->log->append("Error in loading " + *iter + '\n');
+            return;
+        }
+
+    ui->log->append("Initializing successfully\n");
+
+    ui->inputGroupBox->setEnabled(true);
+}
+
+bool MainWindow::loadFile(QString fileName)
+{
+    QFile file(fileName);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return false;
+
+    ui->console->clear();
+
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+        ui->console->append(line);
+    }
+
+    return true;
+}
+
+void MainWindow::on_executeButton_clicked()
+{
+    ui->console->append("=====");
+    ui->console->append(QString("The user has input: "));
+    ui->console->append(ui->input1LineEdit->text());
+}
+
+void MainWindow::on_input1LineEdit_textEdited(const QString &arg1)
+{
+    ui->executeButton->setDisabled(arg1.isEmpty());
+}
+
+void MainWindow::on_actionAboutQt_triggered()
+{
+    qApp->aboutQt();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QToolBar>
+#include <QFileDialog>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QIcon>
 
 namespace Ui {
 class MainWindow;
@@ -15,7 +21,17 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     
+private slots:
+    void on_actionLoad_triggered();
+
+    void on_executeButton_clicked();
+
+    void on_input1LineEdit_textEdited(const QString &arg1);
+
+    void on_actionAboutQt_triggered();
+
 private:
+    bool loadFile(QString);
     Ui::MainWindow *ui;
 };
 
